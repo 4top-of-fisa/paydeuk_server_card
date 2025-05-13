c222563e7d7a1adb4441a49ab73f68d9f6ee7829
eratchacha <papa021326@naver.com>
2025-05-13 16:32:24 +0900

    setting(project): 프로젝트 초기 세팅

---
+++ src/main/java/tower_of_fisa/paydeuk_server_card/PaydeukServerCardApplication.java
@@ -0,0 +1,13 @@
+package tower_of_fisa.paydeuk_server_card;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydeukServerCardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaydeukServerCardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tower_of_fisa/paydeuk_server_card/PaydeukServerCardApplicationTests.java
@@ -0,0 +1,13 @@
+package tower_of_fisa.paydeuk_server_card;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaydeukServerCardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
