a8d74763e0a4fa0654387659f711d77ea9a4cd54
icebear0111 <wltjq0901@naver.com>
2025-05-21 16:47:32 +0900

    chore(swagger): Swagger Error Example 추가 [#10]
    Swagger Error Example 추가
    - 404 에러에 대한 예시 Response 추

--- src/main/java/tower_of_fisa/paydeuk_server_card/common/response/swagger_response/SwaggerResponseExample.java
+++ src/main/java/tower_of_fisa/paydeuk_server_card/common/response/swagger_response/SwaggerResponseExample.java
@@ -15,9 +15,24 @@ public final class SwaggerResponseExample {
             "response": {
               "errorCode": "CC_ERR_01",
               "time": "2024-01-01T00:00:00",
-              "stackTrace": "com.tower_of_fisa.paydeuk_server_service.config.exception.custom.exception.NoSuchElementFoundException404: 해당 카드사가 존재하지 않습니다.",
+              "stackTrace": "tower_of_fisa.paydeuk_server_card.config.exception.custom.exception.NoSuchElementFoundException404: 해당 카드사가 존재하지 않습니다.,
               "errors": null
             }
           }
       """;
+
+  public static final String CARD_404 =
+      """
+              {
+                "success": false,
+                "status": "NOT_FOUND",
+                "message": "카드 정보가 존재하지 않습니다.",
+                "response": {
+                  "errorCode": "CARD_ERR_01",
+                  "time": "2024-01-01T00:00:00",
+                  "stackTrace": "tower_of_fisa.paydeuk_server_card.config.exception.custom.exception.NoSuchElementFoundException404: 카드 정보가 존재하지 않습니다.,
+                  "errors": null
+                }
+              }
+          """;
 }
