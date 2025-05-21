bbc196d595346a5f8acc934910b357579223c462
icebear0111 <wltjq0901@naver.com>
2025-05-21 09:33:28 +0900

    feat(Card Info API): 카드 조회 API 작성 [#5]
    카드 조회 API 작성
    - 전체 카드 조회 API 작성
    - 카드사별 카드 조회 API 작성
    - 레포지토리에 findByCardCompany 메서드 추가

---
+++ src/main/java/tower_of_fisa/paydeuk_server_card/card/controller/CardController.java
@@ -0,0 +1,52 @@
+package tower_of_fisa.paydeuk_server_card.card.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import tower_of_fisa.paydeuk_server_card.card.dto.CardInfoResponse;
+import tower_of_fisa.paydeuk_server_card.card.service.CardService;
+import tower_of_fisa.paydeuk_server_card.common.response.CommonResponse;
+import tower_of_fisa.paydeuk_server_card.domain.enums.CardCompany;
+
+import java.util.List;
+
+@Tag(name = "카드사 API", description = "카드사 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/card")
+@Slf4j
+public class CardController {
+
+  private final CardService cardService;
+
+  @GetMapping("/info")
+  @Operation(summary = "CARD_01 : 전체 카드 조회", description = "모든 카드 정보를 조회합니다.")
+  @ApiResponse(
+      responseCode = "200",
+      description = "카드 정보 조회 성공",
+      content = @Content(schema = @Schema(implementation = CardInfoResponse.class)))
+  public CommonResponse<List<CardInfoResponse>> getAllCardInfo() {
+    List<CardInfoResponse> cardInfoList = cardService.getAllCardInfo();
+    return new CommonResponse<>(true, HttpStatus.OK, "카드 정보 조회 성공", cardInfoList);
+  }
+
+  @GetMapping("/info/{cardCompany}")
+  @Operation(summary = "CARD_02 : 카드사별 카드 조회", description = "카드사별 카드 정보를 조회합니다.")
+  @ApiResponse(
+      responseCode = "200",
+      description = "카드사별 카드 정보 조회 성공",
+      content = @Content(schema = @Schema(implementation = CardInfoResponse.class)))
+  public CommonResponse<List<CardInfoResponse>> getCardInfoByCardCompany(@PathVariable CardCompany cardCompany) {
+    List<CardInfoResponse> cardInfoList = cardService.getCardInfoByCardCompany(cardCompany);
+    return new CommonResponse<>(true, HttpStatus.OK, "카드사별 카드 정보 조회 성공", cardInfoList);
+  }
+}
---
+++ src/main/java/tower_of_fisa/paydeuk_server_card/card/service/CardService.java
@@ -0,0 +1,28 @@
+package tower_of_fisa.paydeuk_server_card.card.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import tower_of_fisa.paydeuk_server_card.card.dto.CardInfoResponse;
+import tower_of_fisa.paydeuk_server_card.card.mapper.CardMapper;
+import tower_of_fisa.paydeuk_server_card.card.repository.CardRepository;
+import tower_of_fisa.paydeuk_server_card.domain.enums.CardCompany;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CardService {
+
+  private final CardRepository cardRepository;
+  private final CardMapper cardMapper;
+
+  public List<CardInfoResponse> getAllCardInfo() {
+    return cardRepository.findAll().stream().map(cardMapper::toDto).toList();
+  }
+
+  public List<CardInfoResponse> getCardInfoByCardCompany(CardCompany cardCompany) {
+    return cardRepository.findByCardCompany(cardCompany).stream().map(cardMapper::toDto).toList();
+  }
+}
