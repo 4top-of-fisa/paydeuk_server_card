10a81b956a0357ef8c951b54e5739de841516be1
sungjoon <dkdldrha@naver.com>
2025-05-22 12:18:03 +0900

    feat(api): 결제 프로세스 api (로직 제외) [#15]
    - 카드 결제 프로세스 controller, service (로직제외)

--- src/main/java/tower_of_fisa/paydeuk_server_card/card/service/CardService.java
+++ src/main/java/tower_of_fisa/paydeuk_server_card/card/service/CardService.java
@@ -5,6 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import tower_of_fisa.paydeuk_server_card.card.dto.CardInfoResponse;
+import tower_of_fisa.paydeuk_server_card.card.dto.PaymentRequest;
+import tower_of_fisa.paydeuk_server_card.card.dto.PaymentResponse;
 import tower_of_fisa.paydeuk_server_card.card.mapper.CardMapper;
 import tower_of_fisa.paydeuk_server_card.card.repository.CardRepository;
 import tower_of_fisa.paydeuk_server_card.domain.enums.CardCompany;
@@ -23,5 +25,11 @@ public class CardService {
 
   public List<CardInfoResponse> getCardInfoByCardCompany(CardCompany cardCompany) {
     return cardRepository.findByCardCompany(cardCompany).stream().map(cardMapper::toDto).toList();
+  }
+
+  public PaymentResponse paymentProcessing(PaymentRequest paymentRequest) {
+    //결제 프로스세를 진행합니다.
+    paymentRequest.getAmount(); // 소나큐브 에러 방지를 위한 대체
+    return new PaymentResponse("2025-05-22-T10:15:30");
   }
 }
