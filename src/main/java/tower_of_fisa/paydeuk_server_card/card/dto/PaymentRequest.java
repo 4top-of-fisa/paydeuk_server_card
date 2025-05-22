c2b075bd3bec96a5289f2c39af2964ad1259368b
sungjoon <dkdldrha@naver.com>
2025-05-22 12:18:33 +0900

    feat(api): 결제 프로세스 api (dto) [#15] - dto

---
+++ src/main/java/tower_of_fisa/paydeuk_server_card/card/dto/PaymentRequest.java
@@ -0,0 +1,19 @@
+package tower_of_fisa.paydeuk_server_card.card.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Schema(title = "PaymentRequest : 카드 결제 [요청 DTO]", description = "요청 데이터")
+public class PaymentRequest {
+    @Schema(description = "카드 토큰", example = "tok_1234567890abcdef")
+    @NotNull(message = "cardToken은 필수입니다.")
+    private String cardToken;
+    @Schema(description = "결제 금액", example = "20000")
+    @NotNull(message = "결제 금액은 필수입니다.")
+    private int amount;
+
+}
