77aaa74d157ada7b830e0cb1eb46b3f0c376b782
icebear0111 <wltjq0901@naver.com>
2025-06-04 11:23:51 +0900

    chore(test): 어노테이션 추가 [#26]
    어노테이션 추가
    - 테스트 하는데 필요한 어노테이션 추가

--- src/main/java/tower_of_fisa/paydeuk_server_card/token/dto/CardIssueRequest.java
+++ src/main/java/tower_of_fisa/paydeuk_server_card/token/dto/CardIssueRequest.java
@@ -1,11 +1,13 @@
 package tower_of_fisa.paydeuk_server_card.token.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
+@AllArgsConstructor
 @Schema(description = "카드 토큰 발급 요청")
 public class CardIssueRequest {
 
